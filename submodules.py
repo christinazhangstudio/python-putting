f69b0a972475f8b237a7b6be315a483da0a17301
Christina Zhang <57076552+christinazhangstudio@users.noreply.github.com>
2024-09-10 14:06:01 -0500

    Create submodules.py

---
+++ submodules.py
@@ -0,0 +1,13 @@
+
+
+
+```
+git submodule add https://github.com/christinazhangstudio/service-api.git ./protos
+git submodule update --remote
+git submodule status
+```
+
+other prereqs
+```
+pip install grpcio-tools
+```
